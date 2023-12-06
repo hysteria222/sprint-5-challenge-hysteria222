dbae08576b350ae90a9fc2c63ce21d7227ba924c
HYSTERIA <129130014+hysteria222@users.noreply.github.com>
2023-12-06 01:32:45 -0600

    i havent pushed in days oops.

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,79 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+      try{
+        const learners ='http://localhost:3003/api/learners'
+        const mentors = 'http://localhost:3003/api/mentors'
+        
+        const resp1 = await axios.get(learners)
+        const resp2 = await axios.get(mentors)
+    
+        const learnersObj = resp1.data
+        const mentorsObj = resp2.data
+
+        const fullArray = learnersObj.map((learner) => {
+          if (!learner.mentors) {
+            return learner;
+          }
+          return {
+            ...learner,
+            mentors: learner.mentors.map((id) => {
+              const mentor = mentorsObj.find((data) => data.id === id);
+              return mentor.firstName + ' ' + mentor.lastName;
+            })
+          };
+        });
+
+        
+        
+        
+
+        const cardMaker = learner => {
+          const card = document.createElement('div');
+          card.classList.add('card');
+          document.querySelector('.cards').appendChild(card)
+          const name = document.createElement('h3');
+          name.textContent = learner.fullName;
+          card.appendChild(name);
+          const email = document.createElement('div');
+          card.appendChild(email);
+          email.textContent = learner.email;
+          const mentorHeading = document.createElement('h4');
+          mentorHeading.textContent = 'Mentors'
+          mentorHeading.classList.add('closed');
+          card.appendChild(mentorHeading);
+
+
+          const mentorList = document.createElement('ul');
+          card.appendChild(mentorList);
+          learner.mentors.forEach((element, i) => {
+          const mentorsListItem = document.createElement('li')
+          mentorsListItem.textContent = learner.mentors[i]
+          mentorList.appendChild(mentorsListItem)
+        })
+          mentorList.style.display = 'none'
+
+         
+          document.querySelector('.info').textContent = 'No learner is selected'
+
+          card.addEventListener('click', () => {
+            name.textContent = learner.fullName + ', ' + 'ID' + ' ' + learner.id;
+            document.querySelector('.info').textContent = `The selected learner is ${learner.fullName}`
+            card.classList.toggle('selected')
+            
+          })
+          
+          return card
+      }
+
+      
+
+      fullArray.forEach(cardMaker)
+
+  } catch (err) {
+    console.log(err.message)
+  }
+  
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
